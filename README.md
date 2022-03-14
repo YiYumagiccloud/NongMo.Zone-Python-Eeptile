c630cf1fb61bbf45899f09c726c74d2f56d7c874
YIYU <yiyumagiccloud@gmail.com>
2022-03-14 10:44:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# NongMo.Zone-Python-Eeptile
+
+一个练手用的 Python 爬虫，针对于 [NongMo.Zone](https://www.ilovexs.com)
+
+输入网址后的 **post_id/xxxxx** 资源路径，即可进行图片的下载
+
+图片默认保存在 D:\ 图片\ 爬到的图片\ NongMo\ 下
+
+多个路径使用空格进行分隔
+
+运行需要下载以下第三方库
+
+---
+1. fake-useragent -> 伪装 UserAgent
+2. requests -> 发起请求
+3. beautifulsoup4 -> HTML 提取器
+4. lxml -> HTML 解析器
+---
+
+使用以下命令进行安装
+
+pip install fake-useragent
+
+pip install requests
+
+pip install beautifulsoup4
+
+pip install lxml
---
+++ 获取链接.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
+from Tools.工具 import 是否有文件夹
+
+UA = {"User-Agent": UserAgent().random}
+
+默认 = 'https://www.ilovexs.com/'
+
+输入 = input('链接路径 :').split(' ')
+
+for 路径 in 输入:
+
+    html_doc = requests.get(f'{默认}{路径}', headers=UA).text
+
+    ADS = BeautifulSoup(html_doc, 'lxml')
+
+    写真名 = ADS.select(".entry-content p")[0].string
+
+    链接 = [i['src'] for i in ADS.select(".entry-content div a img")]
+    
+    去重链接 = list(filter(None, sorted(set(链接),key=链接.index)))
+
+    是否有文件夹('NongMo',f'{写真名}')
+
+    print(f' --- 开始下载 {写真名}, 共{len(去重链接)}张 --- ')
+    print()
+
+    for 几, i in enumerate(去重链接, start=1):
+
+        图片 = requests.get(i, headers=UA, stream=True)
+
+        print(f' --- 正在下载第{几}张 --- ')
+
+        with open(fr'D:\图片\爬到的图片\NongMo\{写真名}\第{几}张.png', 'wb') as F:
+
+            for k in 图片.iter_content(16):
+
+                F.write(k)
+    print()
+    print(f' --- {写真名}, {len(去重链接)}张, 下载完成 --- ')
