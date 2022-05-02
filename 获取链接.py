4a9091b4f050b798893bb10086a67ce9018d5b49
YIYU <yiyumagiccloud@gmail.com>
2022-05-02 22:53:23 +0800

    更新

--- 获取链接.py
+++ 获取链接.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+from tqdm import tqdm
 
-from Tools.工具 import 是否有文件夹
+from Tools.工具 import 创建文件夹
 
 UA = {"User-Agent": UserAgent().random}
 
@@ -22,21 +23,19 @@ for 路径 in 输入:
 
     去重链接 = list(filter(None, sorted(set(链接), key=链接.index)))
 
-    是否有文件夹('NongMo', f'{写真名}')
+    创建文件夹('NongMo', f'{写真名}')
 
-    print(f' --- 开始下载 {写真名}, 共{len(去重链接)}张 --- ')
+    print(f' - 开始下载 {写真名}, 共{len(去重链接)}张 - ')
     print()
 
-    for 几, i in enumerate(去重链接, start=1):
+    for 几, 下载链接 in enumerate(tqdm(去重链接,  desc='下载进度', ncols=69), start=1):
 
-        图片 = requests.get(i, headers={"User-Agent": UserAgent().random}, stream=True)
+        图片 = requests.get(下载链接, headers={"User-Agent": UserAgent().random}, stream=True)
 
-        print(f' --- 正在下载第{几}张 --- ')
+        with open(f'D:\\图片\\爬到的图片\\NongMo\\{写真名}\\第{几}张.png', 'wb') as F:
 
-        with open(fr'D:\图片\爬到的图片\NongMo\{写真名}\第{几}张.png', 'wb') as F:
+            for 数据 in 图片.iter_content(16):
 
-            for k in 图片.iter_content(16):
-
-                F.write(k)
+                F.write(数据)
     print()
-    print(f' --- {写真名}, {len(去重链接)}张, 下载完成 --- ')
+    print(f' - {写真名}, {len(去重链接)}张, 下载完成 - ')
